b7918b25be41c3fb3ab7d840a8623478cae59ce6
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-26 11:51:34 -0300

    add home e novo cliente

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
     return (
         <header className="header w-100 px-3 py-2">
             <nav className="container d-flex justify-content-between align-items-center">
-                <Link>
+                <Link to="/">
                     <img src={logo} alt="SoulPet" />
                 </Link>
                 <div className="d-flex gap-5">

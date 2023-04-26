7c704be4d44ef6ce0f02ff1ea516409e6a83d8b4
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-26 16:32:13 -0300

    add clientes page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Root } from "./pages/Root/Root";
 import { Home } from "./pages/Home/Home";
 import { NovoCliente } from "./pages/NovoCliente/NovoCliente";
+import { Clientes } from "./pages/Clientes/Clientes";
+
 
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Root />}>
           <Route path="/" element={<Home />} />
+          <Route path="/clientes" element={<Clientes />} />
           <Route path="/clientes/novo" element={<NovoCliente />} />
         </Route>
       </Routes>

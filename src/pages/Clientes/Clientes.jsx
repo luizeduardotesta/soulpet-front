c8f157b65a2bcc39f3aabdf23dad210f5541456c
luiztesta <luiz.eduardo.testa.santos@gmail.com>
2023-04-27 12:05:07 -0300

    add editar e deletar clientes

--- src/pages/Clientes/Clientes.jsx
+++ src/pages/Clientes/Clientes.jsx
@@ -1,14 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
+import { toast } from "react-hot-toast";
 
 export function Clientes() {
 
     const [clientes, setClientes] = useState(null);
+    const [show, setShow] = useState(false);
+    const [idCliente, setIdCliente] = useState(null);
+
+    const handleClose = () => {
+        setIdCliente(null);
+        setShow(false)
+    };
+    const handleShow = (id) => {
+        setIdCliente(id);
+        setShow(true)
+    };
 
     useEffect(() => {
+        initializeTable();
+    }, []);
+
+    function initializeTable() {
         axios.get("http://localhost:3001/clientes")
             .then(response => {
                 setClientes(response.data);
@@ -16,7 +32,20 @@ export function Clientes() {
             .catch(error => {
                 console.log(error);
             });
-    }, []);
+    }
+
+    function onDelete() {
+        axios.delete(`http://localhost:3001/clientes/${idCliente}`)
+            .then(response => {
+                toast.success(response.data.message, { position: "bottom-right", duration: 2000 });
+                initializeTable();
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.response.data.message, { position: "bottom-right", duration: 2000 });
+            });
+        handleClose();
+    }
 
     return (
         <div className="clientes container">
@@ -47,10 +76,10 @@ export function Clientes() {
                                         <td>{cliente.email}</td>
                                         <td>{cliente.telefone}</td>
                                         <td className="d-flex gap-2">
-                                            <Button>
+                                            <Button onClick={() => handleShow(cliente.id)}>
                                                 <i className="bi bi-trash-fill"></i>
                                             </Button>
-                                            <Button>
+                                            <Button as={Link} to={`/clientes/editar/${cliente.id}`}>
                                                 <i className="bi bi-pencil-fill"></i>
                                             </Button>
                                         </td>
@@ -60,6 +89,20 @@ export function Clientes() {
                         </tbody>
                     </Table>
             }
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmação</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Tem certeza que deseja excluir o cliente?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={onDelete}>
+                        Excluir
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
---
+++ src/pages/EditaCliente/EditaCliente.jsx
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { useNavigate, useParams } from "react-router-dom";
+
+export function EditaCliente() {
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    function onSubmit(data) {
+        axios.put(`http://localhost:3001/clientes/${id}`, data)
+            .then(response => {
+                toast.success("Cliente cadastrado.", { position: "bottom-left", duration: 2000 });
+                navigate("/clientes");
+            })
+            .catch(error => {
+                toast.error("Algo deu errado.", { position: "bottom-left", duration: 2000 });
+                console.log(error);
+            });
+    }
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/clientes/${id}`)
+            .then(response => {
+                const { nome, email, telefone, endereco: { cidade, uf, cep, rua, numero } } = response.data
+                reset({ nome, email, telefone, endereco: { cidade, uf, cep, rua, numero } })
+            });
+        //    .catch (error => {
+        //    toast.error(error.response.data.message, { position: "bottom-left", duration: 2000 });
+        //    console.log(error);
+        //    navigate("/clientes");
+    }, [id, reset])
+
+    return (
+        <div className="container">
+            <h1>Novo Cliente</h1>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control type="text" className={errors.nome && "is-invalid"} {...register("nome", { required: "O nome é obrigatório", maxLength: { value: 130, message: "Limite de 130 letras" } })} />
+                    {errors.nome && <Form.Text className="invalid-feedback">{errors.nome.message}</Form.Text>}
+
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>E-mail</Form.Label>
+                    <Form.Control type="text" className={errors.email && "is-invalid"} {...register("email", { required: "O email é obrigatório", maxLength: { value: 255, message: "Limite de 255 letras" } })} />
+                    {errors.email && <Form.Text className="invalid-feedback">{errors.email.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Telefone</Form.Label>
+                    <Form.Control type="text" className={errors.telefone && "is-invalid"} {...register("telefone", { required: "O telefone é obrigatório", maxLength: { value: 255, message: "Limite de 255 letras" } })} />
+                    {errors.telefone && <Form.Text className="invalid-feedback">{errors.telefone.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Cidade</Form.Label>
+                    <Form.Control type="text" className={errors.endereco?.cidade && "is-invalid"} {...register("endereco.cidade", { required: "A cidade é obrigatória", maxLength: { value: 255, message: "Limite de 255 letras" } })} />
+                    {errors.endereco?.cidade && <Form.Text className="invalid-feedback">{errors.endereco.cidade.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>UF</Form.Label>
+                    <Form.Control type="text" className={errors.endereco?.uf && "is-invalid"} {...register("endereco.uf", { required: "O Estado é obrigatório", maxLength: { value: 2, message: "Limite de 2 letras" } })} />
+                    {errors.endereco?.uf && <Form.Text className="invalid-feedback">{errors.endereco.uf.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>CEP</Form.Label>
+                    <Form.Control type="text" className={errors.endereco?.cep && "is-invalid"} {...register("endereco.cep", { required: "O CEP é obrigatório", maxLength: { value: 9, message: "Limite de 9 números" } })} />
+                    {errors.endereco?.cep && <Form.Text className="invalid-feedback">{errors.endereco.cep.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Rua</Form.Label>
+                    <Form.Control type="text" className={errors.endereco?.rua && "is-invalid"} {...register("endereco.rua", { required: "A rua é obrigatória", maxLength: { value: 255, message: "Limite de 255 letras" } })} />
+                    {errors.endereco?.rua && <Form.Text className="invalid-feedback">{errors.endereco.rua.message}</Form.Text>}
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Numero</Form.Label>
+                    <Form.Control type="text" className={errors.endereco?.numero && "is-invalid"} {...register("endereco.numero", { required: "A numero é obrigatória", maxLength: { value: 255, message: "Limite de 255 números" } })} />
+                    {errors.endereco?.numero && <Form.Text className="invalid-feedback">{errors.endereco.numero.message}</Form.Text>}
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Editar
+                </Button>
+            </Form>
+        </div>
+    );
+}
